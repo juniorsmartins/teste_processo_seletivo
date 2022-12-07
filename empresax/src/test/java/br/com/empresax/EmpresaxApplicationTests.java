c3d5be8c650645a6d7d92509d28924a577b76bfa
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-12-07 08:58:39 -0400

    Added package projet in Spring Boot.

---
+++ empresax/src/test/java/br/com/empresax/EmpresaxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empresax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
